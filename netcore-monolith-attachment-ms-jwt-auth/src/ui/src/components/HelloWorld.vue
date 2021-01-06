3184c83649400d525802d56fc13aad704a29beed
Antti Stenvall <antti@stenvall.fi>
2021-01-06 12:48:24 +0200

    Vue template is there, next thing is to figure out how to do services

--- netcore-monolith-attachment-ms-jwt-auth/src/ui/src/components/HelloWorld.vue
+++ netcore-monolith-attachment-ms-jwt-auth/src/ui/src/components/HelloWorld.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <!--<h1>{{ msg }}</h1>-->
+    <h1>Welcome!</h1>
+    <p>
+      This is the GUI for attachment microservice demo.
+      Start by logging in!
+    </p>
+    <!--
     <p>            
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <!--
+    </p>    
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
---
+++ netcore-monolith-attachment-ms-jwt-auth/src/ui/src/views/Login.vue
@@ -0,0 +1,38 @@
+<style scoped>
+  input {    
+    margin-right: 20px;
+    }
+</style>
+
+<template>
+  <div class="login">
+    <!--
+    <img alt="Vue logo" src="../assets/logo.png">
+    -->
+    <h1>Login here</h1>
+
+    <input type="text" placeholder="Username">
+    
+    <input type="password" placeholder="Password">
+    
+    <button v-on:click="doLogin">Login</button>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+@Component({
+  components: {    
+  },
+})
+export default class Login extends Vue {
+  doLogin  () {    
+    const url = 'http://localhost:6102/api/auth/login'
+    axios
+      .post(url, {username: 'admin', password: 'demo'})
+      .then(response => console.log(response))
+  }
+}
+</script>
