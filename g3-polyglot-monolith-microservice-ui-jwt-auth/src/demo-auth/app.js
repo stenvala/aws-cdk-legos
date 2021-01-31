7af789b9a0d30fb11b8087871b599a3aa8f249b3
Antti Stenvall <antti@stenvall.fi>
2021-01-31 07:49:12 +0200

    Fix init commands

--- g3-polyglot-monolith-microservice-ui-jwt-auth/src/demo-auth/app.js
+++ g3-polyglot-monolith-microservice-ui-jwt-auth/src/demo-auth/app.js
@@ -1,7 +1,7 @@
-// This is noly for testing what is in the context and event when lambda authorizer is used
+// This is noly for testing what is in the event and context when lambda authorizer is used in API GW
 exports.handler =  async function(event, context) {
-  console.log("CONTEXT: \n" + JSON.stringify(context, null, 2))
   console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  console.log("CONTEXT: \n" + JSON.stringify(context, null, 2))
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/json" },
