e174433c189b6845b523cc85fb7d8a2fb7110940
Antti Stenvall <antti@stenvall.fi>
2021-04-18 15:49:26 +0300

    Better names for stacks

--- g3-ecr-lambda/cdk-ecr/bin/cdk-ecr.ts
+++ g3-ecr-lambda/cdk-ecr/bin/cdk-ecr.ts
@@ -1,21 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CdkEcrStack } from '../lib/cdk-ecr-stack';
+import * as cdk from "@aws-cdk/core";
+import "source-map-support/register";
+import { CdkEcrStack } from "../lib/cdk-ecr-stack";
 
 const app = new cdk.App();
-new CdkEcrStack(app, 'CdkEcrStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new CdkEcrStack(app, "G3-ECRLambda-ECR");
